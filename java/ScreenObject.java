86bda0fb365ec68a0db4ef5611630e95524a5aba
Liam Ellinger <chowder77654@yahoo.com>
2020-02-07 17:01:14 -0600

    Initial ScreenObject Class Commit

---
+++ java/ScreenObject.java
@@ -0,0 +1,28 @@
+import android.media.Image;
+
+public class ScreenObject {
+
+    //screen coordinates
+    int x;
+    int y;
+
+    Image[] imgs; //array of images in the order they are displayed
+    private int currentImgNum;
+    Image currentImg;
+    String imgPath;
+    String name;
+
+    public ScreenObject(String n, String path) {
+        name=n;
+        imgPath=path;
+        currentImgNum = 0;
+
+        //TODO: read in the images from the path
+
+    }
+
+    public void nextImage(){
+        currentImgNum += 1;
+        currentImg = imgs[currentImgNum];
+    }
+}

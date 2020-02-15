c0a4333d003cc685cf82afe23af9b178302efe66
Schafer98 <55207000+Schafer98@users.noreply.github.com>
2020-02-14 18:15:12 -0600

    This is to link to Game View (Schafer)

--- app/src/main/java/com/squidstudios/cryptsandcryptogramsproject/MainActivity.java
+++ app/src/main/java/com/squidstudios/cryptsandcryptogramsproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.squidstudios.cryptsandcryptogramsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        final Button btnNewGame = findViewById(R.id.btnNewGame);
+
+
+
+
+
+
+
+
+        btnNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(MainActivity.this, ___________.class);  //Thus _____ bit is the name of the file Liam created
+                //startActivity(intent);
+            }
+        });
     }
 }

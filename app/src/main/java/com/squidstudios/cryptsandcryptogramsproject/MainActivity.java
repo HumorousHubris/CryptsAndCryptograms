772057bbbf94bb33c6ad0b39add9c3444b3d1c99
Barka-4966 <sydney.pearson@ttu.edu>
2020-02-14 18:59:07 -0600

    Piece together menu, textbox, dungeon screen

--- app/src/main/java/com/squidstudios/cryptsandcryptogramsproject/MainActivity.java
+++ app/src/main/java/com/squidstudios/cryptsandcryptogramsproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.squidstudios.cryptsandcryptogramsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btnNewGame = (Button) findViewById(R.id.btnNewGame);
+        btnNewGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent activityChangeIntent = new Intent(MainActivity.this, TextBox.class);
+                startActivity(activityChangeIntent);
+            }
+        });
+
+        final Button btnLoad = (Button) findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent activityChangeIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(activityChangeIntent);
+            }
+        });
     }
 }

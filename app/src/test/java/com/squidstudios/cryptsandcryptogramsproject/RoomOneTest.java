56ad8271b1750096855ae74ca03c681be61fa4b9
TrishaNag <trisha.nag@outlook.com>
2020-04-03 21:54:05 -0500

    added test file for Room One with empty test functions

---
+++ app/src/test/java/com/squidstudios/cryptsandcryptogramsproject/RoomOneTest.java
@@ -0,0 +1,82 @@
+package com.squidstudios.cryptsandcryptogramsproject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoomOneTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void testOnCreate() {
+    }
+
+    @Test
+    public void addContentView() {
+    }
+
+    @Test
+    public void onStart() {
+    }
+
+    @Test
+    public void onStop() {
+    }
+
+    @Test
+    public void onSaveInstanceState() {
+    }
+
+    @Test
+    public void openOptionsMenu() {
+    }
+
+    @Test
+    public void closeOptionsMenu() {
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+
+    @Test
+    public void startActivityForResult() {
+    }
+
+    @Test
+    public void testStartActivityForResult() {
+    }
+
+    @Test
+    public void startActivity() {
+    }
+
+    @Test
+    public void testStartActivity() {
+    }
+
+    @Test
+    public void startActivities() {
+    }
+
+    @Test
+    public void testStartActivities() {
+    }
+
+    @Test
+    public void testOnCreate1() {
+    }
+}
